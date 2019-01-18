5ce0c95daa08b3bdbe0d7fe819d48c755d41ca22
Adrian Go <adrianashimself@yahoo.com>
2019-01-17 17:11:50 -0800

    Updates

---
+++ precache-manifest.fb767ebaf9d0eb8c1a56e77882e7ab3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "b05afd513fa12ec622af",
+    "url": "/my-app/static/js/main.b05afd51.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/my-app/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "b05afd513fa12ec622af",
+    "url": "/my-app/static/css/main.cde06f38.chunk.css"
+  },
+  {
+    "revision": "a088bf0517994c567d4f51716e7b6027",
+    "url": "/my-app/index.html"
+  }
+];
