03a56334746ccc7ba0ddf5afea05a197588d7d88
Adrian <Driminius@users.noreply.github.com>
2021-10-29 12:42:57 -0700

    Update myweather.js

--- my-weather-app/myweather.js
+++ my-weather-app/myweather.js
@@ -165,7 +165,7 @@
         
         function getLocation() {
           if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition, showError);
+            navigator.geolocation.getCurrentPosition(showPosition);
           } else { 
             x.innerHTML = "Geolocation is not supported by this browser.";
           }
