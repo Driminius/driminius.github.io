ae310b9360f3e2f3c4cf02ccd580dc4994d40dba
Adrian Domingo <adriandomingo@Adrians-MacBook-Pro.local>
2024-08-30 15:16:34 -0700

    added bootstrap framework

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -27,6 +28,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum quidem quisquam dolorem ullam, praesentium delectus adipisci, rerum voluptas sapiente cupiditate officia voluptatem, molestias dignissimos nostrum eaque magnam esse consequuntur saepe!
       </p>
     </>
   )
